ebc1a5ef3c4c3d1781f93f72eac1038e939ae84b
Francis Jacob Taiño <francistaino16@gmail.com>
2022-09-21 19:16:55 +0800

    Added seperate headers and footers.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const e = require('express');
+
 const express = require('express'),
 bodyParser = require('body-parser'),
 ejs = require('ejs'),
 app = express();
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -22,15 +25,23 @@ app.get("/", function(req, res){
 
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    items.push(item);
-    console.log(item);
-    
-    res.redirect("/");
+    if(req.body.list != "Work List"){
+        items.push(item);
+        res.redirect("/");
+    } else {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    console.log(req.body);
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, function(){
